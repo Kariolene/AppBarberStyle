da1ba50a8af79a84b77ac05bb016ee94792a0ff7
Kariolene <karyolene@gmail.com>
2021-04-03 21:32:19 -0300

    Configurado navegaçao

---
+++ App_BarberStyle/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainStack from './src/stacks/MainStack';
+
+export default () => {
+  return (
+     <NavigationContainer>
+       <MainStack/>
+     </NavigationContainer>
+  );
+}
+
